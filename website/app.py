58b48b31607b923b9b4b703be258f3cef7fc284b
Nikita Polovinkin <poloniki@ya.ru>
2023-08-25 02:49:51 +0200

    Initial commit

---
+++ website/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import time
+import pandas as pd
+
+
+@st.cache_data
+def get_data():
+    time.sleep(3)
+    df = pd.read_csv("example.csv")
+    return df
+
+
+st.markdown(
+    """# This is a header
+## This is a sub header
+This is text"""
+)
+
+df = get_data()
+
+# this slider allows the user to select a number of lines
+# to display in the dataframe
+# the selected value is returned by st.slider
+line_count = st.slider("Select a line count", 1, 10, 3)
+
+# and used to select the displayed lines
+head_df = df.head(line_count)
+
+head_df
